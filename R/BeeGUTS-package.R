123d5638bb2126357253c26644b88015fc6370ec
Benoit Goussen <benoit.goussen@ibacon.com>
2020-10-21 15:44:21 +0200

    Initial commit of the BeeGUTS R package

---
+++ R/BeeGUTS-package.R
@@ -0,0 +1,16 @@
+#' The 'BeeGUTS' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name BeeGUTS-package
+#' @aliases BeeGUTS
+#' @useDynLib BeeGUTS, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
+#'
+NULL
