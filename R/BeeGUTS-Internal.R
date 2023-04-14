fb7a5341de7f90276bf62c24a758448ac567f00f
Benoit Goussen <benoit.goussen@ibacon.com>
2023-04-14 15:56:25 +0200

    Workaround to correct a CRAN Note

--- R/BeeGUTS-Internal.R
+++ R/BeeGUTS-Internal.R
@@ -5,4 +5,4 @@ if(getRversion() >= "2.15.1")  utils::globalVariables(c(
   "simQsup95", "time", "q50", "qinf95", "qsup95", "Nsurv_q50_valid",
   "Nsurv_qinf95_valid", "Nsurv_qsup95_valid", "Conc", "Dataset", "Nsurv",
   "qnorm", "qunif", "na.omit", "value", "q_0.025", "q_0.975", "median", "data",
-  "ppcMatching_valid", "SE_id", "id", "hb_value", "approx"))
+  "ppcMatching_valid", "SE_id", "id", "hb_value", "approx", "f_rate", "targConc"))
