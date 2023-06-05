56528c844eacf83124f6895bcebc13f050ce197b
Carlo Romoli <carlo.romoli@ibacon.com>
2023-06-05 10:31:08 +0200

    Improved plotting function. Added documentation

--- R/TimeReinforcedToxicity.R
+++ R/TimeReinforcedToxicity.R
@@ -1,5 +1,5 @@
 #' Perform the extrapolation of the LDD50 to 27 days and check the presence of
-#' reinforced toxicity as for EFSA revised guideline (2023)
+#' reinforced toxicity as for EFSA revised guideline (2023) - Annex G
 #'
 #' When class of \code{object} is \code{beeSurvFit}, see \link[=TRT.beeSurvFit]{TRT.beeSurvFit}.
 #'
@@ -8,41 +8,71 @@
 #' @param object An object used to select a method
 #' @param \dots Further arguments to be passed to generic methods
 #'
-#' @return Result of the TRT check and a graph of the LDD extrapolation compared to the Haber's law
+#' @return A \code{ggplot} object with graph of the LDD extrapolation compared
+#' to the Haber's law and a data.frame with the calculations
+#'
+#' @import ggplot2
+#'
 #' @export
 #'
 TRT <- function(object, ...){
   UseMethod("TRT")
 }
 
-
+#' Calculate the presence of Time Reinforced Toxicity for the compound from the
+#' calibrated model \code{beeSurvFit} object.
+#'
+#' @param object An object of class \code{beeSurvFit}
+#'
+#' @return A object of class \code{ggplot} containing the graph of the comparison
+#' between Haber's law and the predicted lethal doses at 10 and 27 days and a
+#' data.frame with the plotted values.
+#'
+#' @export
+#'
 TRT.beeSurvFit <- function(object){
   if (!is(object,"beeSurvFit")) {
     stop("predict.beeSurvFit: an object of class 'beeSurvFit' is expected")
   }
+  # compute LDD50 at 10 days assuming constant concentration
   LDD50_10 <- LCx(object, X = 50, testType = "Chronic_Oral", timeLCx = 10,
                   concRange = NULL, nPoints = 100)
+  # compute LDD50 at 27 days assuming constant concentration
   LDD50_27 <- LCx(object, X = 50, testType = "Chronic_Oral", timeLCx = 27,
                   concRange = NULL, nPoints = 100)
 
+  # Check for Time Reinforced Toxicity (EFSA, 2023 - Annex G)
   ReinfTox <- LDD50_27$dfLCx$LCx[1] >= (LDD50_10$dfLCx$LCx[1] / 2.7)
 
+  # output on screen
   if (ReinfTox){
-    print("No Time Reinforced Toxicity")
+    cat("No Time Reinforced Toxicity\n")
   } else {
-    print("Time Reinforced Toxicity cannot be excluded")
+    cat("Time Reinforced Toxicity cannot be excluded\n")
   }
 
+  # Fill dataframe with the results
   dfplot <- data.frame(time=c(10,27), ldd50 = c(LDD50_10$dfLCx$LCx[1], LDD50_27$dfLCx$LCx[1]),
                        lddhab=c(LDD50_10$dfLCx$LCx[1], LDD50_10$dfLCx$LCx[1]/2.7),
                        ldd50_q2p5 = c(LDD50_10$dfLCx$LCx[2], LDD50_27$dfLCx$LCx[2]),
                        ldd50_q97p5 = c(LDD50_10$dfLCx$LCx[3], LDD50_27$dfLCx$LCx[3]),
-                       trt = c(1,ReinfTox))
+                       trt = c(NA,as.logical(!ReinfTox)))
 
-  ggplot(dfplot) +
-    geom_errorbar(aes(x=time, y=ldd50, ymin=ldd50_q2p5, ymax=ldd50_q97p5), width=0.05, size=1) +
-    geom_point(aes(x=time, y=ldd50), size=3) +
-    geom_line(aes(x=time, y=lddhab), linetype="solid", color='red', ) +
-    scale_x_log10() +scale_y_log10() + xlab("Time (d)") + ylab("LDD_50 (conc./bee/day)")
+  # show graphical results comparing Haber's law and model predictions
+  trt_plot <- ggplot(dfplot) +
+    geom_errorbar(aes(x=time, y=ldd50, ymin=ldd50_q2p5, ymax=ldd50_q97p5),
+                  width=0.05, linewidth=1) +
+    geom_point(aes(x=time, y=ldd50, color="LDD_50"),size=3) +
+    geom_line(aes(x=time, y=lddhab, color="Haber's law"),
+              linetype="dashed", linewidth=1) +
+    scale_x_log10() +scale_y_log10() + xlab("Time (d)") +
+      ylab("LDD_50 (conc./bee/day)")+
+    guides(color=guide_legend("Legend",
+                              override.aes=list(shape = c(NA,19),
+                                                linetype=c("dashed","solid"))))+
+    scale_color_manual(values = c("red", "black"))
+
+  #return plot and dataframe for further use
+  return(list(trt_plot, dfplot))
 }
 
